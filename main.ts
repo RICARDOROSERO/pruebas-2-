3b34c30a2f2d053711ed97fce31a71a0f5769edb
RICARDOROSERO <117532787+RICARDOROSERO@users.noreply.github.com>
2022-11-05 11:49:37 -0500

    Actualizar main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,38 @@
+input.onButtonPressed(Button.A, function () {
+    basic.showIcon(IconNames.Heart)
+    basic.pause(1000)
+    basic.clearScreen()
+    basic.pause(1000)
+    basic.showIcon(IconNames.Heart)
+})
+input.onButtonPressed(Button.AB, function () {
+    basic.pause(2000)
+    basic.showString("5")
+    basic.pause(2000)
+    basic.showString("4")
+    basic.pause(2000)
+    basic.showString("3")
+    basic.pause(2000)
+    basic.showString("2")
+    basic.pause(2000)
+    basic.showString("1")
+})
+input.onButtonPressed(Button.B, function () {
+    basic.showString("yo")
+    basic.pause(2000)
+    basic.showString("amo")
+    basic.pause(2000)
+    basic.showString("el")
+    basic.pause(2000)
+    basic.showString("cuy")
+})
+basic.showLeds(`
+    # # . # #
+    # . . . #
+    . # . . #
+    . . . . #
+    # # # # #
+    `)
 basic.forever(function () {
 	
 })
